4726b97ce655f33651c6631288f7ab87db1a3aa1
thiagosohsa <thiagosousashp@gmail.com>
2025-03-05 09:08:23 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/thiago/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.thiago.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiago.course.entities.Order;
+import com.thiago.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
