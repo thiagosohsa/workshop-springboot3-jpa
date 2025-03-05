e5afc89621c66e6fe42d5124c461afe06544820c
thiagosohsa <thiagosousashp@gmail.com>
2025-03-05 16:40:46 -0300

    Payment, one-to-one association

--- src/main/java/com/thiago/course/config/TestConfig.java
+++ src/main/java/com/thiago/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.thiago.course.entities.Category;
 import com.thiago.course.entities.Order;
 import com.thiago.course.entities.OrderItem;
+import com.thiago.course.entities.Payment;
 import com.thiago.course.entities.Product;
 import com.thiago.course.entities.User;
 import com.thiago.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
